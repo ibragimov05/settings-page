e394965d8ae8588d9155edf352a8a16e5b445aef
ibragimov05 <ibragimov.asus15@gmail.com>
2024-05-24 18:16:12 +0500

    initial commit

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static ThemeMode themeMode = ThemeMode.light;
+  static String imageUrl =
+      'https://img.freepik.com/free-vector/blur-pink-blue-abstract-gradient-background-vector_53876-174836.jpg';
+  static String language = 'UZ';
+  static Color appColor = Colors.amber;
+
+  static String password = '1111';
+  static Color textColor = Colors.black;
+  static double textSize = 14;
+}
---
+++ lib/views/screens/home_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:settings_page/utils/app_constants.dart';
+import 'package:settings_page/views/widgets/custom_drawer.dart';
+
+class HomeScreen extends StatefulWidget {
+  final ValueChanged<bool> onThemeChanged;
+  final ValueChanged<String> onBackgroundChanged;
+  final ValueChanged<String> onLanguageChanged;
+  final ValueChanged<Color> onColorChanged;
+  final Function(Color, double) onTextChanged;
+
+  const HomeScreen({
+    super.key,
+    required this.onThemeChanged,
+    required this.onBackgroundChanged,
+    required this.onLanguageChanged,
+    required this.onColorChanged,
+    required this.onTextChanged,
+  });
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+            AppConstants.imageUrl,
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        // Make Scaffold background transparent
+        appBar: AppBar(
+          title: Text(
+            "Main page",
+            style: TextStyle(
+              color: AppConstants.textColor,
+              fontSize: AppConstants.textSize,
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Text(AppConstants.language),
+            ),
+          ],
+        ),
+        drawer: CustomDrawer(
+          onThemeChanged: widget.onThemeChanged,
+          onBackgroundChanged: widget.onBackgroundChanged,
+          onLanguageChanged: widget.onLanguageChanged,
+          onColorChanged: widget.onColorChanged,
+          onTextChanged: widget.onTextChanged,
+        ),
+      ),
+    );
+  }
+}
